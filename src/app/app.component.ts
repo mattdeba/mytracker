7094a1ba8dff75e7e0a764edce7e0dc8dd034d57
Matthieu-D <matthieu.debarge@cuma.fr>
2024-11-27 17:27:10 +0100

    ajout du post avec la date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,13 @@ export class AppComponent {
   title = 'mytracker';
   timestamp: string;
 
+  constructor(private http: HttpClient) {
+  }
+
   updateTimestamp() {
     this.timestamp = new Date().toISOString();
+    this.http.post("https://api.sheetbest.com/sheets/f6765ca4-93a9-4612-bf08-48fd77a5fa3e",
+      {poop_date: this.timestamp}
+    ).subscribe();
   }
 }
