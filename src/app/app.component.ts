4b6cca00475176c32c22375438f0f9962ecf1913
Matthieu-D <matthieu.debarge@cuma.fr>
2024-11-27 17:19:49 +0100

    ajout du bouton principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mytracker';
+  timestamp: string;
+
+  updateTimestamp() {
+    this.timestamp = new Date().toISOString();
+  }
 }
